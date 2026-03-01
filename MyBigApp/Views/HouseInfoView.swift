14063c5f3bc317445787e281e2fa49d03290a1b3
Yolanda Piao <ypiao@lcs.on.ca>
2026-03-01 14:43:21 -0500

    Final work

--- MyBigApp/Views/HouseInfoView.swift
+++ MyBigApp/Views/HouseInfoView.swift
@@ -10,22 +10,23 @@ import SwiftUI
 struct HouseInfoView: View {
     var body: some View {
         NavigationStack {
-            VStack {
+            List {
                 HouseView(
-                 houseName: "Ross",
-                 houseInformation: "Opened in October 2022,Ross House is named in honor of the Ross Family For thier signidicant contributions to LCS. This house is part of a modern double residence designed to foster a sense of community on campus."
+                    houseName: "Ross",
+                    houseInformation: "Opened in October 2022,Ross House is named in honor of the Ross Family For their significant contributions to LCS. This house is part of a modern double residence designed to foster a sense of community on campus."
                 )
                 HouseView(
-                 houseName: "Parent",
-                 houseInformation: "Also inaugurated in 2022, Parent House recognizes the Parent Family's leadership and philanthropy. It was named in gratitdue for thier transformative support of the school community."
+                    houseName: "Parent",
+                    houseInformation: "Also inaugurated in 2022, Parent House recognizes the Parent Family's leadership and philanthropy. It was named in gratitdue for thier transformative support of the school community."
                 )
-                 Spacer()
                 
-                .padding()
-                .navigationTitle("LCS Houses")
+                
             }
+            .padding()
+            .navigationTitle("LCS Houses")
         }
-        }
+        
+    }
     
     }
 
