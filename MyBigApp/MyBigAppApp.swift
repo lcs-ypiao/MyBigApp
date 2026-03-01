f917688dc0a8187ff9709fba8ec1983065e941bb
Yolanda Piao <ypiao@lcs.on.ca>
2026-03-01 13:10:08 -0500

    Initial Commit

---
+++ MyBigApp/MyBigAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBigAppApp.swift
+//  MyBigApp
+//
+//  Created by Yolanda Piao on 3/1/26.
+//
+
+import SwiftUI
+
+@main
+struct MyBigAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
